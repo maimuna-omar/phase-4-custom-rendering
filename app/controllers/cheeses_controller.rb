dea37dc9e79269c4b92718d954fede7b9ee6e1f8
maimuna omar <maymunaabdi07@gmail.com>
2023-08-19 13:40:24 +0300

    done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,7 +3,10 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, only: [:created_at, :name]    
+    #render json: cheeses, except: [:created_at, :name]
+
+
   end
 
   # GET /cheeses/:id
